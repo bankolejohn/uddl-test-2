ef54a8e2489b29eefe334e04d7ba0e35dfc43c55
bankolejohn <bankolejohn@gmail.com>
2024-01-09 01:33:18 +0100

    initial commit

---
+++ resig.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+git restore com.epistimis.uddl.query.ide/src-gen/.gitignore
+git restore com.epistimis.uddl.query.ide/xtend-gen/.gitignore
+git restore com.epistimis.uddl.query.tests/src-gen/.gitignore
+git restore com.epistimis.uddl.query.tests/xtend-gen/.gitignore
+git restore com.epistimis.uddl.query.ui.tests/src-gen/.gitignore
+git restore com.epistimis.uddl.query.ui.tests/xtend-gen/.gitignore
+git restore com.epistimis.uddl.query.ui/src-gen/.gitignore
+git restore com.epistimis.uddl.query.ui/xtend-gen/.gitignore
+git restore com.epistimis.uddl.query/src-gen/.gitignore
+git restore com.epistimis.uddl.query/xtend-gen/.gitignore
+git restore com.epistimis.uddl.query.web/src-gen/.gitignore
+git restore com.epistimis.uddl.query.web/xtend-gen/.gitignore
+git status
